bfe1d3295d72b3a870e11814dd145a882fa636da
N21DCDT030LeNgocHuyHoang <156336091+N21DCDT030LeNgocHuyHoang@users.noreply.github.com>
2024-03-20 09:04:45 +0700

    Delete N21DCDT030_LeNgocHuyHoang directory

--- N21DCDT030_LeNgocHuyHoang/bai_6.py
+++
@@ -1,9 +0,0 @@
-def fibonancci(n):
-    if(n==1 or n==2):
-        return 1
-    else:
-        return fibonancci(n-1)+fibonancci(n-2)
-print(fibonancci(12))
-n=int(input("nhap n:"))
-for i in range(1,n+1,1):
-    print(fibonancci(i),end="   ")
